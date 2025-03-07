712a6f18527878727fddb8972f27e607a2a7cb61
Adilkhan <barakadik2@gmail.com>
2025-03-07 23:02:49 +0500

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sizcontrol.app',
+  appName: 'СИЗ.Контрол',
+  webDir: 'dist/apps/siz-control-mobile/browser',
+};
+
+export default config;
